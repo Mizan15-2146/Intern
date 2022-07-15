066cd922062959cc0c427b7861c9dac6846436f4
Mizan15-2146 <mizanur1221rahman@gmail.com>
2022-07-15 20:21:00 +0600

    Using Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RightSideComponent } from './right-side/right-side.component';
+import { LeftSideComponent } from './left-side/left-side.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RightSideComponent,
+    LeftSideComponent
   ],
   imports: [
     BrowserModule,
